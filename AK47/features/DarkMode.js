332da58080c6778ce3dad4c9e74d3d292dfec5b4
satnnyy <satnnyy@gmail.com>
2025-05-17 08:33:31 +1000

    fix

--- AK47/features/DarkMode.js
+++
@@ -1,6 +0,0 @@
-import config from "../config"
-
-register("renderOverlay", () => {
-    if (!config().DarkMode) return
-    Renderer.drawRect(Renderer.color(0, 0, 0, config().DarkModeOpacity), 0, 0, Renderer.screen.getWidth(), Renderer.screen.getHeight())
-})
